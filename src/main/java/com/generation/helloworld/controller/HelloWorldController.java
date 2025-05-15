ee7f2431f5cac7f264a89078ed9dc9c3e9fefd22
Helena <maria.helen@live.com>
2025-05-15 16:36:26 -0300

    Remoção de Comentários e ajuste Identação

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController  // Anotação que indica que essa classe é um controlador REST
-@RequestMapping("/hello") //td requisicao q vier pra hello, é 
-							//essa controladora q vai decidir oq fazer com ela
-
+@RestController  
+@RequestMapping("/hello") 
 public class HelloWorldController {
 	
 	
-    @GetMapping("/hello")  // Anotação que mapeia a URL "/hello" para esse método
+    @GetMapping("/hello")  
 	public String hello() {
 		return "<b>Hello World!</b>";
 	}
